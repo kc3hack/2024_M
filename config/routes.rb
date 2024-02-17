23d820767254a3c42393db0f79810c9ab2f06963
hamuhamu5hamu <hamuh5266@gmail.com>
2024-02-17 22:28:01 +0900

    add
    googlemapを表示しました。

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :roots, only: [:index, :new]
-  get 'root/index'
+
+  get 'root/result' => 'root#result'
 
   root to: 'root#index'
 end
