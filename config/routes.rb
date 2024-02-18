fcab22f5383f94eafd029ef63e877f1804c4ff3f
fwnty <aztia0705@gmail.com>
2024-02-18 21:15:07 +0900

    add

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :roots, only: [:index, :new]
+  #resources :routes, only: [:index, :new, :edit, :create]
+  resources :routes
 
   get 'root/result' => 'root#result'
 
