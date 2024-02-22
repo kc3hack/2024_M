3d86897ab040d6ec786e89a74e2787ad2450365b
atnuhs <syunnta0033@gmail.com>
2024-02-22 14:35:52 +0900

    ログイン機能(email必須、CSSなし)

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,4 @@
+class UsersController < ApplicationController
+    def index  #追加
+    end  #追加
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_02_18_165118) do
+ActiveRecord::Schema.define(version: 2024_02_20_084933) do
 
   create_table "courses", force: :cascade do |t|
     t.text "start"
     t.text "destination"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.text "waypoints", default: ""
+    t.text "waypoints"
   end
 
   create_table "routes", force: :cascade do |t|
@@ -26,6 +26,19 @@ ActiveRecord::Schema.define(version: 2024_02_18_165118) do
     t.text "waypoints"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "name"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
