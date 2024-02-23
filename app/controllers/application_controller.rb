34c91ae53916c48d218c275d7f1e60cc41e98342
hamuhamu5hamu <hamuh5266@gmail.com>
2024-02-24 00:55:34 +0900

    fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,7 +18,7 @@ class ApplicationController < ActionController::Base
     before_action :configure_permitted_parameters, if: :devise_controller?
 
     def after_sign_in_path_for(resource)
-        courses_mypageHistory_path
+        mypage_path
     end
     
     def after_sign_out_path_for(resource)
