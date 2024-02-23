03eb258343a1fb207b007d9e0addcfb245100067
hamuhamu5hamu <hamuh5266@gmail.com>
2024-02-23 15:46:08 +0900

    fix

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  root 'courses#siteTop'
+  root 'courses#siteTop' 
 
   get 'courses/siteTop' => 'courses#siteTop'
 
